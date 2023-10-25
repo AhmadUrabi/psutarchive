3acd6e78b19f64e563f0f0d188953662bc504625
HDDTHR <stevenjildeh@gmail.com>
2023-10-25 17:30:58 +0300

    chore: fix eslintrc file to work correctly

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,34 @@
 module.exports = {
   root: true,
+  extends: [
+    "@nuxt/eslint-config",
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+  ],
   plugins: ["prettier"],
-  extends: ["@nuxt/eslint-config", "plugin:prettier/recommended"],
   rules: {
+    eqeqeq: "error",
+    "no-console": "warn",
     "prettier/prettier": "error",
   },
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: "latest",
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jest: true,
+  },
+  ignorePatterns: [
+    "node_modules",
+    "build",
+    "dist",
+    "public",
+    ".nuxt",
+    ".vscode",
+  ],
   overrides: [
     {
       files: ["*.vue"],
