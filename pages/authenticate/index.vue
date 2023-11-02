1cf3029a9d90ceac1e87d24542ac2b2c356b403e
HDDTHR <stevenjildeh@gmail.com>
2023-11-02 23:21:57 +0300

    feat: add logic to save jwt and go to home page

--- pages/authenticate/index.vue
+++ pages/authenticate/index.vue
@@ -7,22 +7,39 @@
 <script setup lang="ts">
 // The google authentication button on the upload page will redirect to this page after a successful login.
 // It will include a code query that needs to be traded for a jwt from the api.
+import { storeJwt } from '@/utils/jwt';
 const route = useRoute()
 const router = useRouter()
 
+const goToHome = () => {
+    router.replace("/");
+}
+
 onMounted(() => {
     const query = route.query;
+    console.log(query)
 
     if (!query?.code) {
-        router.replace("/");
+        goToHome();
         return;
     }
 
-    // Trade code with jwt https://market.strapi.io/plugins/strapi-google-auth
+    const response = fetch("https://api.psutarchive.com/strapi-google-auth/user-profile", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            code: query.code,
+        })
+    }).then((res) => {
+        if (res.status != 200) {
+            return;
+        }
+        res.json().then((response) => { storeJwt(response.data.token) })
+    })
 
-    // If code is valid store response jwt in localstorage with storeJwt
-
-    // If invalid, invalidateJwt() then redirect to home page
+    goToHome();
 })
 </script>
 <style scoped></style>
