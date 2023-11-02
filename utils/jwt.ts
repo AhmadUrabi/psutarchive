217e473c797d9b31458bf2de976159c7fa1bbee7
HDDTHR <stevenjildeh@gmail.com>
2023-11-02 16:51:10 +0300

    feat: add utils to manage jwt

---
+++ utils/jwt.ts
@@ -0,0 +1,21 @@
+const parseJwt = (token: string) => {
+    return JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString());
+}
+
+export const isJwtExpired = (token: string) => {
+    const parsedToken = parseJwt(token);
+    const current_time = new Date().getTime() / 1000;
+    if (current_time > parsedToken.exp)
+        return true;
+    return false;
+}
+
+export const invalidateJwt = () => {
+    const STORAGE_KEY = "JWT";
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+export const storeJwt = (token: string) => {
+    const STORAGE_KEY = "JWT";
+    localStorage.setItem(STORAGE_KEY, token);
+}
